9cbfb01d021f6f7f3f168c0e1ed46ecd3b227d45
Luiz Faria <127424248+devfaria@users.noreply.github.com>
2024-05-16 19:27:50 -0300

    Correção do código
    Feito a correção do código

--- script.js
+++ script.js
@@ -21,7 +21,6 @@ function renderElements(tasks) {
   });
 }
 
-
 function createTaskItem(task) {
   const li = document.createElement('li');
   li.className = 'task__item';
@@ -32,19 +31,18 @@ function createTaskItem(task) {
   const taskType = document.createElement('span');
   taskType.className = 'task-type';
 
-
   switch (task.type.toLowerCase()) {
     case 'urgente':
       taskType.classList.add('span-urgent');
+      taskType.setAttribute('aria-label', 'Urgente');
       break;
     case 'importante':
       taskType.classList.add('span-important');
+      taskType.setAttribute('aria-label', 'Importante');
       break;
     case 'normal':
       taskType.classList.add('span-normal');
-      break;
-    default:
-
+      taskType.setAttribute('aria-label', 'Normal');
       break;
   }
 
@@ -57,9 +55,13 @@ function createTaskItem(task) {
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
     <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/>
   </svg>
-`;
+  `;
   removeButton.addEventListener('click', () => {
-    li.remove();
+    const taskIndex = tasks.indexOf(task);
+    if (taskIndex > -1) {
+      tasks.splice(taskIndex, 1);
+    }
+    renderElements(tasks);
   });
 
   taskInfoContainer.appendChild(taskType);
@@ -77,8 +79,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const titleInput = document.getElementById('input_title').value;
     const typeSelect = document.querySelector('.form__input--priority').value;
 
-    if (titleInput) {
-
+    if (titleInput && typeSelect) {
       const newTask = { title: titleInput, type: typeSelect };
 
       tasks.push(newTask);
@@ -87,10 +88,9 @@ document.addEventListener('DOMContentLoaded', function () {
       document.getElementById('input_title').value = '';
       document.querySelector('.form__input--priority').value = '';
     } else {
-
-      alert('Por favor, digite um título para a tarefa.');
+      alert('Por favor, digite um título e selecione um tipo para a tarefa.');
     }
   });
-});
 
-renderElements(tasks);
+  renderElements(tasks);
+});
